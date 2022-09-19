2d359b55fc2e146a916704d777169a1c5d8a60c9
mehediakash <hacker.aklogic@gmail.com>
2022-09-20 00:55:52 +0600

    Done

---
+++ js/js.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  $(".sidenav").sidenav();
+});
+
+// slider
+$(document).ready(function () {
+  $(".slider").slider();
+});
+
+// metrarial boxed
+$(document).ready(function () {
+  $(".materialboxed").materialbox();
+});
+
+$(document).ready(function () {
+  $(".carousel").carousel();
+});
